69a8af3df275015eb2ac395bf89cf329758a7437
hunterwaller <102311048+hunterwaller@users.noreply.github.com>
2022-06-15 10:30:04 -0400

    Add files via upload

---
+++ Fetching Data.jsx
@@ -0,0 +1,157 @@
+const Pagination = ({ items, pageSize, onPageChange, onChange }) => { // Destructuring requires line 136+
+  const { Button } = ReactBootstrap;
+  if (items.length <= 1) return null;
+
+  let num = Math.ceil(items.length / pageSize);
+  let pages = range(1, num);
+  const list = pages.map(page => {
+    return (
+      <Button key={page} onClick={onPageChange} className="page-item">
+        {page}
+      </Button>
+    );
+  });
+  return (
+    <nav>
+      <ul className="pagination">{list}</ul>
+    </nav>
+  );
+};
+const range = (start, end) => {
+  return Array(end - start + 1)
+    .fill(0)
+    .map((item, i) => start + i);
+};
+function paginate(items, pageNumber, pageSize) {
+  const start = (pageNumber - 1) * pageSize;
+  let page = items.slice(start, start + pageSize);
+  return page;
+}
+const useDataApi = (initialUrl, initialData) => {
+  const { useState, useEffect, useReducer } = React;
+  const [url, setUrl] = useState(initialUrl);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: initialData
+  });
+
+  useEffect(() => {
+    let didCancel = false;
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_INIT" });
+      try {
+        const result = await axios(url);
+        if (!didCancel) {
+          dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+        }
+      } catch (error) {
+        if (!didCancel) {
+          dispatch({ type: "FETCH_FAILURE" });
+        }
+      }
+    };
+    fetchData();
+    return () => {
+      didCancel = true;
+    };
+  }, [url]);
+  return [state, setUrl];
+};
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      };
+    case "FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      };
+    default:
+      throw new Error();
+  }
+};
+// App that gets data from Hacker News url
+function App() {
+  const { Fragment, useState, useEffect, useReducer } = React;
+  const [query, setQuery] = useState("MIT");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    "https://hn.algolia.com/api/v1/search?query=MIT",
+    {
+      hits: []
+    }
+  );
+  const onChange = (event) => {
+    if (Number(event.target.value) <= 4) {
+      setPageSize(5);
+    } else {
+      setPageSize(Number(event.target.value));
+    }
+  }
+  const handlePageChange = e => {
+    setCurrentPage(Number(e.target.textContent));
+  };
+  let page = data.hits;
+  if (page.length >= 1) {
+    page = paginate(page, currentPage, pageSize);
+    console.log(`currentPage: ${currentPage}`);
+  }
+  return (
+    <Fragment>
+      <form
+        onSubmit={event => {
+          doFetch("http://hn.algolia.com/api/v1/search?query=${query}");
+          event.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <ul>
+          {page.map(item => (
+            <li className="list-group" key={item.objectID}>
+              <a className="list-group-item" href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+      <Pagination // Needs to be matched due to thte line 1 destructuring
+        items={data.hits}
+        pageSize={pageSize}
+        onPageChange={handlePageChange}
+        onChange={onChange}
+      ></Pagination>
+      <form>
+        <input type="number" width="100" min="5" onChange={onChange}></input>
+      </form>
+    </Fragment>
+  );
+}
+
+// ========================================
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ README.md
@@ -0,0 +1,10 @@
+# Fetching Data Exercise
+
+<h2>Description:</h2>
+<p>This program obtains data from a website and displays it on the page in a list.  This list has multiple pages and allows the user to change how many list items are on the page at once.</p>
+
+<h2>How to Run:</h2>
+<p>After installing the files, use command propmt to move into the folder directory, then run a server.  The page will be loaded on localhost:8080</p>
+
+<h2>Roadmap:</h2>
+<p>There will be a submit button for the items per page selector.  Currently it automatically updates as the value is changed, making the page very jumpy.</p>
